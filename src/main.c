e1ccce544613070f67871b4f675e84ce0df3aea8
Tristan Brookes <tristan@tristanbrook.es>
2015-06-20 18:38:07 +0100

    Added in random wow text

--- src/main.c
+++ src/main.c
@@ -3,9 +3,27 @@
 static Window *s_main_window;
 static Layer *s_canvas_layer;
 static TextLayer *s_time_layer;
+static TextLayer *s_wow_layer;
 static GFont s_time_font;
+static GFont s_wow_font;
+static int s_random;
 
 static GDrawCommandImage *s_command_image;
+
+static void wow_text(){
+	s_random = rand() % 3;
+	
+	if(s_random == 0){
+		text_layer_set_text(s_wow_layer,"such time");
+		text_layer_set_text_color(s_wow_layer, GColorFashionMagenta);
+	} else if (s_random == 1){
+		text_layer_set_text(s_wow_layer,"very pebble");
+		text_layer_set_text_color(s_wow_layer, GColorCyan);
+	} else if (s_random == 2){
+		text_layer_set_text(s_wow_layer,"much watch");
+		text_layer_set_text_color(s_wow_layer, GColorGreen);
+	}
+}
 
 static void update_time(){
 	
@@ -26,6 +44,7 @@ static void update_time(){
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
 	update_time();
+	wow_text();
 }
 
 static void update_proc(Layer *layer, GContext *ctx) {
@@ -68,9 +87,26 @@ static void window_load(Window *window) {
   	// Improve the layout to be more like a watchface
   	text_layer_set_font(s_time_layer, s_time_font);
   	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+	
+	// Improve the layout to be more like a watchface
+  	text_layer_set_font(s_time_layer, s_time_font);
 
   	// Add it as a child layer to the Window's root layer
   	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+	
+	// Create wow layer
+	s_wow_layer = text_layer_create(GRect(10,136,124,50));
+	text_layer_set_background_color(s_wow_layer, GColorClear);
+  	text_layer_set_text_color(s_wow_layer, GColorBlack  );
+	
+	// create gfont
+  	s_wow_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_COMIC_NEUE_BOLD_20));
+	
+	// Improve the layout to be more like a watchface
+  	text_layer_set_font(s_wow_layer, s_wow_font);
+	
+	// Add it as a child layer to the Window's root layer
+  	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_wow_layer));
 }
 
 static void window_unload(Window *window) {
@@ -82,15 +118,17 @@ static void window_unload(Window *window) {
 	
 	// destroy text layer
 	text_layer_destroy(s_time_layer);
+	text_layer_destroy(s_wow_layer);
 	
 	// unload gfont
 	fonts_unload_custom_font(s_time_font);
+	fonts_unload_custom_font(s_wow_font);
 }
 
 static void init() {
     // Set up main Window
     s_main_window = window_create();
-    window_set_background_color(s_main_window, GColorYellow);
+    window_set_background_color(s_main_window, GColorMayGreen);
     window_set_window_handlers(s_main_window, (WindowHandlers) {
         .load = window_load,
         .unload = window_unload,
@@ -100,9 +138,11 @@ static void init() {
 	
 	// make sure the time is displayed from the start
 	update_time();
+	wow_text();
 	
 	// register with ticktimerservice
 	tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+	//tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 }
 
 static void deinit() {
